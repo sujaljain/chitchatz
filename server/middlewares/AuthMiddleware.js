2fa83f05da75c38f17f98dcc78c168d1a106aa02
sujaljain <sujaljain1911@gmail.com>
2025-04-06 01:27:33 +0530

    Initial Commit

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -1,21 +1,21 @@
 /**
-1️⃣ What is Middleware?
-✅ Middleware functions in Express.js are functions that execute before reaching the final request handler.
-✅ They have access to the request (req), response (res), and next function (next()).
-
-📌 Think of middleware as a security checkpoint at an airport:
-
-Middleware 1: Security checks your passport.
-Middleware 2: You go through baggage screening.
-Middleware 3: You pass through a metal detector.
-Final Destination (Route Handler): If all checks pass, you board the flight.
-🔹 In Express, middleware works the same way:
-
-It runs before the route handler.
-It modifies the request or response.
-It decides whether to pass control (next()) or block the request.
-
-*/
+ 1️⃣ What is Middleware?
+ ✅ Middleware functions in Express.js are functions that execute before reaching the final request handler.
+ ✅ They have access to the request (req), response (res), and next function (next()).
+ 
+ 📌 Think of middleware as a security checkpoint at an airport:
+ 
+ Middleware 1: Security checks your passport.
+ Middleware 2: You go through baggage screening.
+ Middleware 3: You pass through a metal detector.
+ Final Destination (Route Handler): If all checks pass, you board the flight.
+ 🔹 In Express, middleware works the same way:
+ 
+ It runs before the route handler.
+ It modifies the request or response.
+ It decides whether to pass control (next()) or block the request.
+ 
+ */
 
 import jwt from "jsonwebtoken";
 
